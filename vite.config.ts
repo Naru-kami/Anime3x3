a8cf76f44f909be92f34c364777b1a30ff82468e
KevinHelders <91275265+Naru-kami@users.noreply.github.com>
2023-09-05 17:15:01 +0200

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: { chunkSizeWarningLimit: 4000, },
   plugins: [react()],
+  base: '/Anime3x3/',
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
